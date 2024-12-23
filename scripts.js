9f13f4ce774ba7f64ea22b907f3c78eba15826d9
pedepro <administrativo@pedepro.com.br>
2024-12-23 02:44:30 -0300

    landing

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function subscribe() {
+    alert("Obrigado por assinar o PrintPro! Comece a usar agora mesmo.");
+}
+
+function sendMessage(event) {
+    event.preventDefault();
+    alert("Mensagem enviada com sucesso! Entraremos em contato em breve.");
+}
